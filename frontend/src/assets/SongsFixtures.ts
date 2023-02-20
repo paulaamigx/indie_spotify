5932a2558485109aa2fa378e75265fbd1e2ff63a
pauamigx <paulaamigo22@gmail.com>
2023-02-20 10:39:15 -0300

    chore: add more albums to fixture

--- frontend/src/assets/SongsFixtures.ts
+++ frontend/src/assets/SongsFixtures.ts
@@ -4,20 +4,32 @@ import { Lenna } from '.'
 export const Albums: IAlbum[] = [
   {
     id: '0',
-    title: 'album0',
-    artist: 'artist0',
+    title: 'Album0',
+    artist: 'Artist0',
     cover: Lenna,
   },
   {
     id: '1',
-    title: 'album1',
-    artist: 'artist1',
+    title: 'Album1',
+    artist: 'Artist1',
     cover: Lenna,
   },
   {
     id: '2',
-    title: 'album2',
-    artist: 'artist2',
+    title: 'Album2',
+    artist: 'Artist2',
+    cover: Lenna,
+  },
+  {
+    id: '3',
+    title: 'Album3',
+    artist: 'Artist3',
+    cover: Lenna,
+  },
+  {
+    id: '4',
+    title: 'Album4',
+    artist: 'Artist4',
     cover: Lenna,
   }
 ]
